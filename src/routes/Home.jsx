c8d08ee8bbf23a43e6684fa7156a0abd81c1fd9c
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-06-03 23:17:12 -0300

    Pegando os dados usado o get do axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import blogFetch from "../axios/config";
+
 export function Home() {
+  const [posts, setPost] = useState([]);
+
+  const getPost = async () => {
+    try {
+      const response = await blogFetch.get('/posts')
+      const data = response.data
+      setPost(data)
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+
+    </div>
   )
 }
